a2ff0d454925bc3a25f85c8f89c5eeecd549db38
josedaniel <josedaniel@htm.com.co>
2017-05-06 17:55:30 -0500

    ya escucha el socket io

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const path = require('path');
-const app = express();
-const port = 5000;
-const host = '0.0.0.0';
+const host = 'localhost';
+const port = 5001;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+//app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
 
 app.get('/pruebas', (req, res)=> { 
     var hora = Date();
     res.send('Hola Mundo, la hora es: '  + hora);
 });
 
-app.listen(port, host, ()=>{
+io.on('connection', function (socket) {
+    console.log("nuevo cliente conectado");
+  socket.emit('Room 1', { hello: 'world' });
+  socket.on('Room Test', function (data) {
+    console.log(data);
+  });
+});
+
+server.listen(port, host, ()=>{
     console.log(`Listening in http://${host}:${port}`);
 });
