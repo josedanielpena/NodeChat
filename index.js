d799279e0f4e22859a1d9832cfd1243e968e938f
josedaniel <josedaniel@htm.com.co>
2017-05-06 16:50:57 -0500

    ya se agrega el public

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
 const host = '0.0.0.0';
 
-app.get('/', (req, res)=> { 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/pruebas', (req, res)=> { 
     var hora = Date();
     res.send('Hola Mundo, la hora es: '  + hora);
 });
